6998c8943d6238ca97ea458f8a74eb15e467cf44
crispynap <crispynap@gmail.com>
2018-05-18 19:12:46 +0900

    변경: 필요없는 파일들 제거

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,13 @@
 $(document).ready(() => {
 
-
+  $(function () {
+    var socket = io();
+    $('form').submit(function () {
+      socket.emit('chat message', $('#m').val());
+      $('#m').val('');
+      return false;
+    });
+  });
 
   $('#new_session_btn').click(() => {
     $('.session_set').addClass('off');
--- server.js
+++ server.js
@@ -11,31 +11,19 @@ app.set("views", __dirname + "/server/views");
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
-app.use("/", index);
-
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
-io.on("connection", function (socket) {
-  console.log("a user connected");
-  socket.broadcast.emit("hi");
-  socket.on("disconnect", function () {
-    console.log("user disconnected");
-  });
-});
+app.use("/", index);
 
-io.on("connection", function (socket) {
-  socket.on("chat message", function (msg) {
-    console.log("message: " + msg);
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
   });
 });
 
 http.listen(3000, function () {
-  console.log("listening on *:3000");
+  console.log('listening on *:3000');
 });
 
-io.on("connection", function (socket) {
-  socket.on("chat message", function (msg) {
-    io.emit("chat message", msg);
-  });
-});
