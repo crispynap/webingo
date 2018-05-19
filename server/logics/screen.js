72817416eb7f577a42e5321b9095ed12944383d8
crispynap <crispynap@gmail.com>
2018-05-20 01:15:28 +0900

    변경: 세션을 소켓에 저장하는 것으로

--- server/logics/screen.js
+++ server/logics/screen.js
@@ -5,36 +5,18 @@ module.exports = function (io, sessions) {
 
   io.on('connection', function (socket) {
 
-    const startScripts = [
-      { type: 'clear' },
-      { type: 'msg big', msg: `빈고 게임에 참여한 여러분을 환영합니다.` },
-      { type: 'msg', msg: `이 게임에서 여러분은 빈고의 활동가가 됩니다.` },
-      { type: 'msg', msg: `함께 빈고를 운영하며 빈고를 익히는 것이 이 게임의 목표입니다.` },
-      { type: 'msg', msg: `그럼 시작해봅시다.` },
-      { type: 'clear' },
-      { type: 'msg', msg: `20XX년, 여러분은 작은 은행을 하나 만듭니다.`, more: true },
-      { type: 'set bank' },
-      { type: 'msg', msg: `빈집이라는 주거 공동체들의 전월세 보증금을 모으기 위해서였습니다.`, more: true },
-      { type: 'function', type: 'set communes', func: () => 'a' },
-      { type: 'msg', msg: `여러분 중에는 빈집에 살고 있는 사람도 있었고,` },
-      { type: 'msg', msg: `빈집에 놀러오는 사람이나 관심이 있는 사람도 있었으며,` },
-      { type: 'msg', msg: `빈집은 잘 모르지만 이 은행의 취지에 공감한 사람도 있었습니다.` },
-      { type: 'msg', msg: `여러분은 이 은행의 이름을 빈마을 금고라는 의미에서 <b>빈고</b>라 지었습니다.`, more: true },
-      { type: 'set bank name to bingo' },
-    ];
-
     const emit = (event, data) => { socket.emit(event, data) };
     const timeoutEmit = (event, data, time = 0) => setTimeout(() => emit(event, data), time);
-    const mainDpMsg = (data, time = 0) => timeoutEmit('main message', data, time);
-    const mainDpMsgBig = (data, time = 0) => timeoutEmit('main message big', data, time);
-    const mainDpClear = () => timeoutEmit('main clear');
+    const screenMsg = (data, time = 0) => timeoutEmit('main message', data, time);
+    const screenMsgBig = (data, time = 0) => timeoutEmit('main message big', data, time);
+    const screenClear = () => timeoutEmit('main clear');
 
     const genScriptsQueue = function* (scripts) {
       for (let script of scripts) {
         switch (script.type) {
-          case 'clear': mainDpClear(script.time); break;
-          case 'msg': mainDpMsg(script.data, script.time); break;
-          case 'msg big': mainDpMsgBig(script.data, script.time); break;
+          case 'clear': screenClear(script.time); break;
+          case 'msg': screenMsg(script.data, script.time); break;
+          case 'msg big': screenMsgBig(script.data, script.time); break;
           case 'function': timeoutEmit(script.event, script.func(), script.time); break;
           default: timeoutEmit(script.event, script.data, script.time);
         }
@@ -42,6 +24,28 @@ module.exports = function (io, sessions) {
         yield;
       };
     }
+
+    const getCommunes = () => {
+      return 'a'
+    }
+
+    const startScripts = [
+      { type: 'clear' },
+      { type: 'msg big', data: `빈고 게임에 참여한 여러분을 환영합니다.` },
+      { type: 'msg', data: `이 게임에서 여러분은 빈고의 활동가가 됩니다.` },
+      { type: 'msg', data: `함께 빈고를 운영하며 빈고를 익히는 것이 이 게임의 목표입니다.` },
+      { type: 'msg', data: `그럼 시작해봅시다.` },
+      { type: 'clear' },
+      { type: 'msg', data: `20XX년, 여러분은 작은 은행을 하나 만듭니다.`, more: true },
+      { type: 'set bank' },
+      { type: 'msg', data: `빈집이라는 주거 공동체들의 전월세 보증금을 모으기 위해서였습니다.`, more: true },
+      { type: 'function', event: 'set communes', func: getCommunes },
+      { type: 'msg', data: `여러분 중에는 빈집에 살고 있는 사람도 있었고,` },
+      { type: 'msg', data: `빈집에 놀러오는 사람이나 관심이 있는 사람도 있었으며,` },
+      { type: 'msg', data: `빈집은 잘 모르지만 이 은행의 취지에 공감한 사람도 있었습니다.` },
+      { type: 'msg', data: `여러분은 이 은행의 이름을 빈마을 금고라는 의미에서 <b>빈고</b>라 지었습니다.`, more: true },
+      { type: 'set bank name to bingo' },
+    ];
 
     socket.on('session start', () => {
       socket.scriptQueue = genScriptsQueue(startScripts);
--- server/logics/sessions.js
+++ server/logics/sessions.js
@@ -4,72 +4,61 @@ module.exports = function (io) {
   let sessions = {
     list: [],
     add(session) { this.list.push(session); },
-    remove(sessionId) {
-      this.list = _.filter(this.list, session => session.getSessionId() !== sessionId &&
-        session.getClientId() !== sessionId);
+
+    remove(id) {
+      this.list = _.filter(this.list, session => session.num !== id && session.clientId !== id);
     },
-    getSession(sessionId) {
-      return _.find(this.list, session => session.getSessionId() == sessionId ||
-        session.getClientId() === sessionId);
+
+    getSession(id) {
+      return _.find(this.list, session => session.num == id || session.clientId === id);
     }
   };
 
   class Session {
     constructor(clientId) {
-      this._clientId = clientId;
-      this._sessionId = genSessionNum();
-      this._players = [];
-      this._game = new Game();
+      this.clientId = clientId;
+      this.num = genSessionNum();
+      this.players = [];
+      this.game = new Game();
     }
 
-    getClientId() { return this._clientId }
-    getSessionId() { return this._sessionId }
-    addPlayer(player) { this._players.push(player) }
+    addPlayer(player) { this.players.push(player); }
     getPlayer(idOrNick) {
-      return _.find(this._players, player =>
-        player.getId() == idOrNick || player.getNick() == idOrNick
-      )
+      return _.find(this.players, player => player.id == idOrNick || player.nick == idOrNick);
     }
-    getPlayersNick() { return _.map(this._players, player => player._nick); }
-    getPlayers() { return this._players; }
-    removePlayer(playerId) { this._players = _.filter(this._players, player => player.getId() !== playerId); }
+    getPlayersNick() { return _.map(this.players, player => player.nick); }
+    removePlayer(id) { this.players = _.filter(this.players, player => player.clientId !== id); }
   }
 
   class Player {
     constructor(clientId, nick) {
-      this._clientId = clientId;
-      this._nick = nick;
+      this.clientId = clientId;
+      this.nick = nick;
     }
-
-    getId() { return this._clientId; }
-    getNick() { return this._nick; }
 
   }
 
   class Game {
     constructor() {
-      this._charactors = [];
-      this._bingo = new Bingo();
+      this.charactors = [];
+      this.bingo = new Bingo();
     }
 
   }
 
   class Charactor {
     constructor(id, nick) {
-      this._id = id;
-      this._nick = nick;
+      this.id = id;
+      this.nick = nick;
     }
   }
 
   class Bingo {
     constructor(id, nick) {
-      this._id = id;
-      this._nick = nick;
-      this._communes = [];
+      this.id = id;
+      this.nick = nick;
+      this.communes = [];
     }
-
-    addCommune(commune) { this._communes.push(commune) }
-    getCommunes() { return this._communes; }
 
   }
 
@@ -80,47 +69,44 @@ module.exports = function (io) {
         return;
       }
 
-      if (socket.sessionId) {
-        const session = sessions.getSession(socket.sessionId);
-        session.removePlayer(socket.id);
-        io.to(socket.sessionId).emit('player changed', session.getPlayersNick());
+      if (socket.session) {
+        socket.session.removePlayer(socket.id);
+        io.to(socket.session.num).emit('player changed', socket.session.getPlayersNick());
       }
     });
 
     socket.on('new session', function () {
       const session = new Session(socket.id);
       sessions.add(session);
-      sessionId = session.getSessionId();
-      socket.sessionId = sessionId;
-      socket.join(sessionId);
-      socket.emit('set session number', sessionId);
+      socket.session = session;
+      socket.join(session.num);
+      socket.emit('set session number', session.num);
     });
 
     socket.on('close session', function () {
       sessions.remove(socket.id);
-      io.to(socket.sessionId).emit('session closed');
-      socket.sessionId = undefined;
+      io.to(socket.session.num).emit('session closed');
+      socket.session = undefined;
     });
 
-    socket.on('new player', function (nick, sessionId) {
-      const session = sessions.getSession(sessionId);
-      console.log(session)
-      console.log(session.getPlayer(nick));
+    socket.on('new player', function (nick, sessionNum) {
+      const session = sessions.getSession(sessionNum);
+
       if (!session) { socket.emit('no session'); return; }
       if (session.getPlayer(nick)) { socket.emit('duplicated nick'); return; }
 
       session.addPlayer(new Player(socket.id, nick));
-      socket.sessionId = sessionId;
-      socket.join(sessionId);
-      socket.emit('player added', sessionId);
-      io.to(sessionId).emit('player changed', session.getPlayersNick());
+      socket.session = session;
+      socket.join(session.num);
+      socket.emit('player added', session.num);
+      io.to(session.num).emit('player changed', session.getPlayersNick());
     });
 
     socket.on('session exit', () => {
-      const session = sessions.getSession(socket.sessionId)
+      const session = socket.session;
       session.removePlayer(socket.id);
-      io.to(socket.sessionId).emit('player changed', session.getPlayersNick());
-      socket.sessionId = undefined;
+      io.to(session.num).emit('player changed', session.getPlayersNick());
+      socket.session = undefined;
     });
   });
 
@@ -131,8 +117,6 @@ module.exports = function (io) {
     return sessionNum;
   };
   const genNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-
 
   return sessions;
 }
