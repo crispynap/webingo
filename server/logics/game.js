9064475558c69aa851d9e0af0bdab28f2d073ec7
crispynap <crispynap@gmail.com>
2018-05-19 17:50:16 +0900

    추가: emit 함수들 제작

--- server/logics/game.js
+++ server/logics/game.js
@@ -4,6 +4,8 @@ module.exports = function (io, sessions) {
 
   io.on('connection', function (socket) {
 
+    let game;
+
     const getThisSession = () => sessions.getSession(socket.sessionId);
 
     class Charactor {
@@ -28,18 +30,30 @@ module.exports = function (io, sessions) {
       }
     }
 
-    const sendMainMsg = (msg, time = 0) => setTimeout(() => socket.emit('main message', msg), time);
-    const sendMainMsgBig = (msg, time = 0) => setTimeout(() => socket.emit('main message big', msg), time);
-    const sendMainButton = (label, time = 0) => setTimeout(() => socket.emit('set main button', label), time);
+    const emit = (event, msg) => socket.emit(event, msg);
+    const timeoutEmit = (event, msg, time = 0) => setTimeout(() => emit(event, msg), time);
+    const mainDpMsg = (msg, time = 0) => timeoutEmit('main message', msg, time);
+    const mainDpMsgBig = (msg, time = 0) => timeoutEmit('main message big', msg, time);
+    const mainDpButton = (label, time = 0) => timeoutEmit('set main button', label, time);
+    const mainDpClear = (time = 0) => timeoutEmit('set main button', label, time);
 
     socket.on('session start', () => {
-      const game = new Game();
-      sendMainMsgBig('빈고 게임에 참여한 것을 환영합니다.', 100);
-      sendMainMsg('이 게임에서 여러분은 빈고의 활동가가 됩니다.', 300);
-      sendMainMsg('함께 빈고를 운영하며 빈고를 익히는 것이 이 게임의 목표입니다.', 500);
-      sendMainMsg('그럼 시작해봅시다.', 1000);
-      sendMainButton('시작한다!', 1200);
+      game = new Game();
+      mainDpMsgBig('빈고 게임에 참여한 여러분을 환영합니다.', 100);
+      mainDpMsg('이 게임에서 여러분은 빈고의 활동가가 됩니다.', 600);
+      mainDpMsg('함께 빈고를 운영하며 빈고를 익히는 것이 이 게임의 목표입니다.', 1100);
+      mainDpMsg('그럼 시작해봅시다.', 1600);
+      mainDpButton('시작한다!', 2000);
+    });
 
+    socket.on('game start', () => {
+      const game = new Game();
+      mainDpClear();
+      mainDpMsgBig('빈고 게임에 참여한 여러분을 환영합니다.', 100);
+      mainDpMsg('이 게임에서 여러분은 빈고의 활동가가 됩니다.', 600);
+      mainDpMsg('함께 빈고를 운영하며 빈고를 익히는 것이 이 게임의 목표입니다.', 1100);
+      mainDpMsg('그럼 시작해봅시다.', 1600);
+      mainDpButton('시작한다!', 2000);
     });
 
   });
